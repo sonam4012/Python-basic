641fc6066955f1d18c79dfcf698abf65573ef944
Sonam Kumari <sonammishra1204@gmail.com>
2026-09-21 23:49:17 +0530

    lopps in python

---
+++ lecture_five.py
@@ -0,0 +1,69 @@
+# i = 0
+
+# while i <= 100 :
+#    print(i)
+#    i = i+1
+
+
+# i = 100
+
+# while i >= 1 :
+#    print(i)
+#    i = i-1
+
+
+# num = int(input("enter the number"))
+
+# i = 1
+# while(i <= 10):
+#   print(num * i)
+#   i = i + 1
+
+
+
+# nums = [1,2,4,16,25,36,49]
+
+# idx = 0
+
+# while(idx < len(nums)):
+#     print(nums[idx])
+#     idx = idx + 1
+
+
+
+# nums = (1,2,3,4,5,6)
+
+# x = 36
+# i = 0
+
+# while i < len(nums):
+#     if(nums[i] == x):
+#         print(i)
+#     i = i + 1
+
+
+# list = [1,2,3,4,4]
+
+# for num in list:
+#     print(num)
+
+
+# tuple = (1,2,3,4,5,6,7)
+# idx = 0
+# for el in tuple:
+#     if(el == 3):
+#         print("number found :", idx)
+#         idx+= 1
+
+
+# for el in range(1,101,1):
+#     print(el)
+
+# for el in range(101,0,-1):
+#     print(el)4
+
+
+# n = int(input("Enter the number: "))
+
+# for el in range(1, 11):
+#     print(n * el)
